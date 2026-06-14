efd221baae506aee9574c0b9d42936fb21bd2ee4
burgues0 <victordpp@outlook.com>
2026-06-14 00:28:35 -0300

    add: start script pro floci

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 set -e
 
-cd localstack
+cd floci
 docker compose up -d --wait
 
 cd ../terraform
@@ -11,4 +11,10 @@ cd ../ansible
 ansible-playbook -i hosts.ini api.yaml
 ansible-playbook -i hosts.ini gateway.yaml
 
+EC2_CONTAINER=$(docker ps --format '{{.Names}}' | grep floci-ec2)
+EC2_IP=$(docker inspect $EC2_CONTAINER --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+
+socat TCP-LISTEN:5000,fork,reuseaddr TCP:$EC2_IP:5000 &
+
+sleep 2
 curl -s http://localhost:5000/usuarios
