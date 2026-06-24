1e328f042659dd2b4647262f3a0907194e0cecb9
burgues0 <victordpp@outlook.com>
2026-06-23 21:09:25 -0300

    add: DinD container simulation

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -2,10 +2,10 @@ output "gateway_public_ip" {
     value = aws_instance.gateway.public_ip
 }
 
-output "backend_public_ip" {
-    value = aws_instance.backend.public_ip
+output "backend_private_key_path" {
+    value = local_file.backend_private_key.filename
 }
 
-output "private_key_path" {
-    value = local_sensitive_file.private_key.filename
+output "backend_port" {
+    value = 2301
 }
